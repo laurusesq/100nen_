a53bd5bb0e9e140a520f505b2593384c7f2afc73
laurusesq <laurus-@ezweb.ne.jp>
2025-04-25 00:04:42 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,26 +1,23 @@
-
 self.addEventListener('install', function(event) {
-    console.log('[ServiceWorker] Installed');
     event.waitUntil(
         caches.open('100nen').then(function(cache) {
             return cache.addAll([
                 '/',
                 '/index.html',
+                '/script.js',
+                '/style.css',
                 '/manifest.json',
-                '/icon.png',
-                '/style.css'
+                '/icon.png'
             ]);
         })
     );
 });
 
 self.addEventListener('activate', function(event) {
-    console.log('[ServiceWorker] Activated');
     event.waitUntil(self.clients.claim());
 });
 
 self.addEventListener('fetch', function(event) {
-    console.log('[ServiceWorker] Fetch', event.request.url);
     event.respondWith(
         caches.match(event.request).then(function(response) {
             return response || fetch(event.request);
