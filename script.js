e893dd0859520efa404617fdb60f6f72f310bd03
laurusesq <laurus-@ezweb.ne.jp>
2025-05-04 14:58:22 +0900

    Update script.js

--- script.js
+++ script.js
@@ -419,7 +419,7 @@ function extractUniqueTags(articles) {
 
   // 件数の多い順 → 同じなら五十音順（日本語対応）でソート
   return Object.entries(tagCounts).sort((a, b) => {
-    if (b[1] !== a[1]) return b[1] - a[1];
+    if (b[1] !== a[1]) return a[1] - b[1];
     return a[0].localeCompare(b[0], 'ja', { numeric: false, sensitivity: 'base', ignorePunctuation: true});
   });
 }
@@ -427,9 +427,6 @@ function extractUniqueTags(articles) {
 const selectedTags = new Set();
 
 function renderTagButtons(tagsWithCounts) {
-
-  console.log(tagsWithCounts.map(([tag, count]) => `${tag} (${count})`).join("\n"));
-
   const container = document.getElementById("tag-buttons");
   container.innerHTML = "";
 
